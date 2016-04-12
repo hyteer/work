b54a66ea848cbfe54eaf40ce1fe33f0a72fcf89e
hyteer <hyteer@qq.com>
2016-04-13 00:32:59 +0800

    Java21

---
+++ Java/Projects/Java21/src/Week2/CodeKeeper2.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week2;
+import java.util.*;
+/**
+ *
+ * @author Administrator
+ */
+public class CodeKeeper2 {
+    ArrayList<String> list;
+    String[] codes = { "alpha", "lambda", "gamma", "delta", "zeta" };
+    
+    public CodeKeeper2(String[] userCodes) {
+        list = new ArrayList<>();
+        // load built-in codes
+        for (int i=0; i<codes.length; i++) {
+            addCode(codes[i]);
+        }
+        // load user codes
+        for (int j=0; j<userCodes.length; j++) {
+            addCode(userCodes[j]);
+        }
+        // display all codes
+        for (Iterator ite = list.iterator(); ite.hasNext();) {
+            String output = (String) ite.next();
+            System.out.println(output);
+        }
+        
+        for ( Object name: list) {
+            System.out.println("|"+name);
+        }
+    }
+    
+    private void addCode(String code) {
+        if (!list.contains(code)) {
+            list.add(code);
+        }
+    }
+    
+    public static void main(String[] args) {
+        CodeKeeper2 keeper = new CodeKeeper2(args);
+    }
+}
